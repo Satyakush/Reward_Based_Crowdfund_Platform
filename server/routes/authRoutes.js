dc6a16b804617f329318a16054026ae56a0eb25e
Satyam Kushwaha <satykush123456789@gmail.com>
2026-09-25 20:38:14 +0530

    Fix campaign API and authentication

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,11 +1,10 @@
-// server/routes/authRoutes.js
-
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/authController');
+const { registerUser, loginUser, getCurrentUser } = require('../controllers/authController');
+const { protect } = require('../middleware/authMiddleware');
 
-// Define routes
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.get('/me', protect, getCurrentUser);
 
 module.exports = router;
